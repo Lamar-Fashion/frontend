aeac26b2d2ad70059858865573fd9f4e28dcbd0d
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-11-25 15:43:16 +0200

    finished shop section version 1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React from 'react'
-import Header from './components/Header/Header'
-import Home from "./components/Home/Home"
+import React from 'react';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
-import {
-  BrowserRouter as Router,
-  Routes ,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-    <Router>
-      
+      <Router>
+        <Header />
 
-      <Header/>
-     
-<Routes >
-<Route exact path="/" element={<Home/>}/>
-</Routes>
-<Footer/>
-    </Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Home/shop/Shop.jsx
+++ src/components/Home/shop/Shop.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import "../../../home-styles/shop.css"
+import React from 'react';
+import '../../../home-styles/shop.css';
+import shop1 from '../../../images/shop/shop1.jpeg';
+import shop2 from '../../../images/shop/shop2.jpeg';
+import shop3 from '../../../images/shop/shop3.jpeg';
+import shop4 from '../../../images/shop/shop4.jpeg';
 function Shop() {
-    return (
-        <>
-            <section className="shop">
-
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className='shop'>
+        <div className='bigContainer'>
+          <section className='leftSection'>
+            <img src={shop1} alt='' className='shopImg img1' />
+            <img src={shop2} alt='' className='shopImg img2' />
+            <img src={shop3} alt='' className='shopImg img3' />
+            <img src={shop4} alt='' className='shopImg img4' />
+          </section>
+          <section className='rightSection'>
+            <h3 className='text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore fugiat enim perferendis rerum reiciendis quos hic atque, sunt qui libero doloremque distinctio alias modi quis at,
+              laborum quam dolorem. Similique.
+            </h3>
+            <button className='button shopBtn'>Shop Now</button>
+          </section>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Shop
+export default Shop;
