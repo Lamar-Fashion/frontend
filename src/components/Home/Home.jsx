ff6350f614e0f51f730ea48a8a64238cc65281f1
abu-nofal <anofal719@gmail.com>
2021-11-27 15:37:20 +0200

    feadback section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Brand from './brand/Brand'
 import Hero from './hero/Hero'
+import People from './people/People'
 import Products from './products/Products'
 import Shop from './shop/Shop'
 
@@ -13,6 +14,7 @@ function Home() {
         <Brand/>
         <Shop/>
         <Products/>
+        <People/>
            
         </>
     )
