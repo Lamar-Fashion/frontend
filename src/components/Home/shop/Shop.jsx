adc4c31ed411bd42737ed39d4280f23a97d099e3
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-11-27 15:39:12 +0200

    fixed navbar

--- src/components/Home/shop/Shop.jsx
+++ src/components/Home/shop/Shop.jsx
@@ -9,18 +9,18 @@ function Shop() {
     <>
       <section className='shop'>
         <div className='bigContainer'>
-          <section className='leftSection'>
-            <img src={shop1} alt='' className='shopImg img1' />
-            <img src={shop2} alt='' className='shopImg img2' />
-            <img src={shop3} alt='' className='shopImg img3' />
-            <img src={shop4} alt='' className='shopImg img4' />
-          </section>
           <section className='rightSection'>
             <h3 className='text'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore fugiat enim perferendis rerum reiciendis quos hic atque, sunt qui libero doloremque distinctio alias modi quis at,
               laborum quam dolorem. Similique.
             </h3>
             <button className='button shopBtn'>Shop Now</button>
+          </section>
+          <section className='leftSection'>
+            <img src={shop1} alt='' className='shopImg img1' />
+            <img src={shop2} alt='' className='shopImg img2' />
+            <img src={shop3} alt='' className='shopImg img3' />
+            <img src={shop4} alt='' className='shopImg img4' />
           </section>
         </div>
       </section>
