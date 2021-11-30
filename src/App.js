2cd19e59a89e3af922dab6f3c9e8d4bcbbe8e225
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-11-30 17:55:37 +0200

    turn off shop

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
 function App() {
   return (
     <>
       <Router>
-        {/* <Header /> */}
+        <Header />
 
         <Routes>
           <Route exact path='/' element={<Home />} />
