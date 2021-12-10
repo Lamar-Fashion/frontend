751b2585709056746ed05be922f1f4070e0b2de9
abu-nofal <anofal719@gmail.com>
2021-12-10 15:21:29 +0200

    first commit in cards

--- src/components/Abaya/abaya-cards/AbayaCards.jsx
+++ src/components/Abaya/abaya-cards/AbayaCards.jsx
@@ -1,11 +1,228 @@
 import React from 'react';
 import '../../../styles/abaya-styles/abaya-cards.css';
+import lamar from "../../../images/brand/test/brand12.jpeg"
+import neo from "../../../images/brand/test/brand2.jpeg"
 function AbayaCards() {
   return (
     <>
       <section className='abaya-cards'>
         <div className='lamar-container'>
-          <div className='box'></div>
+          <div className='box'>
+            <div className="over-view">
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="go-view">
+              <i class="far fa-eye"></i>
+              </div>
+            </div>
+            <div className="overlay"></div>
+            <div className="image">
+            <img src={lamar} alt="" className="img-product"/>
+         <div className="shadow"></div>
+            </div>
+            <div className="info">
+                <h3>lamar</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <div className="price">
+                  <span className="size">small</span>
+                  <span className="price-p">QAR 1200</span>
+                </div>
+                <button className="add-cart"> add to cart </button>
+            </div>
+          </div>
+          <div className='box'>
+            <div className="over-view">
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="go-view">
+              <i class="far fa-eye"></i>
+              </div>
+            </div>
+            <div className="overlay"></div>
+            <div className="image">
+            <img src={lamar} alt="" className="img-product"/>
+         <div className="shadow"></div>
+            </div>
+            <div className="info">
+                <h3>lamar</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <div className="price">
+                  <span className="size">small</span>
+                  <span className="price-p">QAR 1200</span>
+                </div>
+                <button className="add-cart"> add to cart </button>
+            </div>
+          </div>
+          <div className='box'>
+            <div className="over-view">
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="go-view">
+              <i class="far fa-eye"></i>
+              </div>
+            </div>
+            <div className="overlay"></div>
+            <div className="image">
+            <img src={lamar} alt="" className="img-product"/>
+         <div className="shadow"></div>
+            </div>
+            <div className="info">
+                <h3>lamar</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <div className="price">
+                  <span className="size">small</span>
+                  <span className="price-p">QAR 1200</span>
+                </div>
+                <button className="add-cart"> add to cart </button>
+            </div>
+          </div>
+          <div className='box'>
+            <div className="over-view">
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="go-view">
+              <i class="far fa-eye"></i>
+              </div>
+            </div>
+            <div className="overlay"></div>
+            <div className="image">
+            <img src={neo} alt="" className="img-product"/>
+         <div className="shadow"></div>
+            </div>
+            <div className="info">
+                <h3>neo</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <div className="price">
+                  <span className="size">small</span>
+                  <span className="price-p">QAR 1200</span>
+                </div>
+                <button className="add-cart"> add to cart </button>
+            </div>
+          </div>
+          <div className='box'>
+            <div className="over-view">
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="go-view">
+              <i class="far fa-eye"></i>
+              </div>
+            </div>
+            <div className="overlay"></div>
+            <div className="image">
+            <img src={lamar} alt="" className="img-product"/>
+         <div className="shadow"></div>
+            </div>
+            <div className="info">
+                <h3>lamar</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <div className="price">
+                  <span className="size">small</span>
+                  <span className="price-p">QAR 1200</span>
+                </div>
+                <button className="add-cart"> add to cart </button>
+            </div>
+          </div>
+          <div className='box'>
+            <div className="over-view">
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="go-view">
+              <i class="far fa-eye"></i>
+              </div>
+            </div>
+            <div className="overlay"></div>
+            <div className="image">
+            <img src={neo} alt="" className="img-product"/>
+         <div className="shadow"></div>
+            </div>
+            <div className="info">
+                <h3>neo</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <div className="price">
+                  <span className="size">small</span>
+                  <span className="price-p">QAR 1200</span>
+                </div>
+                <button className="add-cart"> add to cart </button>
+            </div>
+          </div>
+          <div className='box'>
+            <div className="over-view">
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="go-view">
+              <i class="far fa-eye"></i>
+              </div>
+            </div>
+            <div className="overlay"></div>
+            <div className="image">
+            <img src={neo} alt="" className="img-product"/>
+         <div className="shadow"></div>
+            </div>
+            <div className="info">
+                <h3>neo</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <div className="price">
+                  <span className="size">small</span>
+                  <span className="price-p">QAR 1200</span>
+                </div>
+                <button className="add-cart"> add to cart </button>
+            </div>
+          </div>
+          <div className='box'>
+            <div className="over-view">
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="fav">
+              <i class="fas fa-heart"></i>
+              </div>
+              <div className="go-view">
+              <i class="far fa-eye"></i>
+              </div>
+            </div>
+            <div className="overlay"></div>
+            <div className="image">
+            <img src={lamar} alt="" className="img-product"/>
+         <div className="shadow"></div>
+            </div>
+            <div className="info">
+                <h3>lamar</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <div className="price">
+                  <span className="size">small</span>
+                  <span className="price-p">QAR 1200</span>
+                </div>
+                <button className="add-cart"> add to cart </button>
+            </div>
+          </div>
         </div>
       </section>
     </>
