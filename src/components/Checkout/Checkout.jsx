8644ee98f55be84948c65c2da24102a75f1729e4
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2022-01-16 20:41:42 +0200

    some editings

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,22 +1,22 @@
-import {React,useState} from 'react'
-import { Link } from "react-router-dom";
-import "../../styles/checkout/checkout.css"
+import { React, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../styles/checkout/checkout.css';
 import { useNavigate } from 'react-router-dom';
 
 function Checkout() {
-  const cartArray= JSON.parse(window.sessionStorage.getItem("cart"))
-  const total= JSON.parse(window.sessionStorage.getItem("total"))
-  const [showAnswer, setShowAnswer] = useState({email:false,phone:false})
+  const cartArray = JSON.parse(window.sessionStorage.getItem('cart'));
+  const total = JSON.parse(window.sessionStorage.getItem('total'));
+  const [showAnswer, setShowAnswer] = useState({ email: false, phone: false });
   const navigate = useNavigate();
-  let [values, setValues] = useState(JSON.parse(window.sessionStorage.getItem('checkout_person_info'))?JSON.parse(window.sessionStorage.getItem('checkout_person_info')):{});
+  let [values, setValues] = useState(JSON.parse(window.sessionStorage.getItem('checkout_person_info')) ? JSON.parse(window.sessionStorage.getItem('checkout_person_info')) : {});
 
   const handleChange = (e) => {
     setValues((values) => ({ ...values, [e.target.name]: e.target.value }));
   };
-  function handleSubmit (e){
+  function handleSubmit(e) {
     e.preventDefault();
     window.sessionStorage.setItem('checkout_person_info', JSON.stringify(values));
-    navigate("/Checkout2");
+    navigate('/Checkout2');
     window.scrollTo({
       left: 0,
       top: 0,
@@ -24,164 +24,145 @@ function Checkout() {
     });
   }
 
-    return (
-        <>
-        <section className="checkout" >
-          
-
-        <div className="nav-container" >
-      <div className="nav-info">
-          <div className="left-nav">
-            <Link to='/'><i className="fas fa-home i-home"></i></Link>  
-            <i className="fas fa-angle-right"></i> <Link to='/Cart' className="exat-path"> <span >cart</span></Link> 
-            <i className="fas fa-angle-right"></i> <span >Shipping</span>
+  return (
+    <>
+      <section className='checkout'>
+        <div className='nav-container'>
+          <div className='nav-info'>
+            <div className='left-nav'>
+              <Link to='/'>
+                <i className='fas fa-home i-home'></i>
+              </Link>
+              <i className='fas fa-angle-right'></i>{' '}
+              <Link to='/Cart' className='exat-path'>
+                {' '}
+                <span>cart</span>
+              </Link>
+              <i className='fas fa-angle-right'></i> <span>Shipping</span>
+            </div>
           </div>
-         
         </div>
-      </div>
-        <div className="lamar-container">
-                <h2>
-                  <span className='active'>Shipping <span className='steps'>1</span>
-                  </span> 
-                  <i className="fas fa-angle-right"></i> 
-                  <span> Payments <span className='steps'>2</span>
-                  </span>
-                  <i className="fas fa-angle-right"></i> 
-                  <span>Order Complete <span className='steps'>3</span>
-                  </span>
-                  </h2>
-          <div className="container-shiping">
-          <form action="" onSubmit={handleSubmit}>
-            
-                
-              <div className="email">
-              <i className="fas fa-mail-bulk"></i>
-            <input type="email" name="email" id="email" placeholder="Email Address *" 
-            value={values?.email} required onChange={handleChange}/>
-              <i className="far fa-question-circle question" onClick={()=>{
-                setShowAnswer({email:!showAnswer.email})
-              }}>
-                {
-                  showAnswer.email&&<div className='answer-q'>We'll send your order confirmation here.</div>
-                }
-
+        <div className='lamar-container'>
+          <h2>
+            <span className='active'>
+              Shipping <span className='steps'>1</span>
+            </span>
+            <i className='fas fa-angle-right'></i>
+            <span>
+              {' '}
+              Payments <span className='steps'>2</span>
+            </span>
+            <i className='fas fa-angle-right'></i>
+            <span>
+              Order Complete <span className='steps'>3</span>
+            </span>
+          </h2>
+          <div className='container-shiping'>
+            <form action='' onSubmit={handleSubmit}>
+              <div className='email'>
+                <i className='fas fa-mail-bulk'></i>
+                <input type='email' name='email' id='email' placeholder='Email Address *' value={values?.email} required onChange={handleChange} />
+                <i
+                  className='far fa-question-circle question'
+                  onClick={() => {
+                    setShowAnswer({ email: !showAnswer.email });
+                  }}
+                >
+                  {showAnswer.email && <div className='answer-q'>We'll send your order confirmation here.</div>}
                 </i>
               </div>
-              <div className="phone">
-              <i className="fas fa-phone"></i>
-            <input type="tel" name="phone" id="phone" placeholder="Phone Number *" required onChange={handleChange}
-            value={values?.phone}/>
-             <i className="far fa-question-circle question" onClick={()=>{
-                setShowAnswer({phone:!showAnswer.phone})
-              }}> 
-              {
-                showAnswer.phone&&
-             <div className='answer-q'>For delivery questions.</div>
-              }
-               
-             </i>
+              <div className='phone'>
+                <i className='fas fa-phone'></i>
+                <input type='tel' name='phone' id='phone' placeholder='Phone Number *' required onChange={handleChange} value={values?.phone} />
+                <i
+                  className='far fa-question-circle question'
+                  onClick={() => {
+                    setShowAnswer({ phone: !showAnswer.phone });
+                  }}
+                >
+                  {showAnswer.phone && <div className='answer-q'>For delivery questions.</div>}
+                </i>
               </div>
-              <div className="name">
-                <div className="Fname">
-                  <i className="fas fa-user-edit"></i>
-                <input type="text" name="Fname" id="Fname" 
-                value={values?.Fname} placeholder="First Name *" required  onChange={handleChange}/ >
-
+              <div className='name'>
+                <div className='Fname'>
+                  <i className='fas fa-user-edit'></i>
+                  <input type='text' name='Fname' id='Fname' value={values?.Fname} placeholder='First Name *' required onChange={handleChange} />
                 </div>
-                <div className="Lname">
-                  <i className="fas fa-user-edit"></i>
-                <input type="text" name="Lname" id="Lname" value={values?.Lname} placeholder="Last Name *" required onChange={handleChange}/>
-
-                </div>
-  
-              </div>
-              <div className="country">
-              <i className="fas fa-globe-africa"></i>
-            <input type="text" name="country" id="country" placeholder="country" value="Qatar" />
-              </div>
-              <div className="city">
-              <i className="fas fa-city"></i>
-            <input type="text" name="city" id="city" placeholder="City *" required onChange={handleChange}
-             value={values?.city}/>
-              </div>
-              <div className="zone">
-              <i className="fas fa-city"></i>
-            <input type="text" name="Zone" id="Zone" placeholder="Zone  *"  value={values.Zone? values.Zone : ""} onChange={handleChange}/>
-              </div>
-              <div className="address">
-                <div className="Faddress">
-                <i className="fas fa-map-marked-alt"></i>
-                <input type="text" name="Faddress" id="Faddress" placeholder="Street Address *" required onChange={handleChange} 
-                value={values?.Faddress}/ >
-
-                </div>
-                <div className="Laddress">
-                <i class="fas fa-building"></i>
-                <input type="text" name="Laddress" id="Laddress" placeholder="Flat / Villa Number  *" onChange={handleChange}
-                value={values?.Laddress?values.Laddress:"" } />
-
+                <div className='Lname'>
+                  <i className='fas fa-user-edit'></i>
+                  <input type='text' name='Lname' id='Lname' value={values?.Lname} placeholder='Last Name *' required onChange={handleChange} />
                 </div>
               </div>
-              
-              
-              <button className="next" type='submit' > 
-              {/* <Link to="/Checkout2" >Next</Link> */}
-              Next
+              <div className='country'>
+                <i className='fas fa-globe-africa'></i>
+                <input type='text' name='country' id='country' placeholder='country' value={values?.country} required onChange={handleChange} />
+              </div>
+              <div className='city'>
+                <i className='fas fa-city'></i>
+                <input type='text' name='city' id='city' placeholder='City *' required onChange={handleChange} value={values?.city} />
+              </div>
+              <div className='zone'>
+                <i className='fas fa-city'></i>
+                <input type='text' name='Zone' id='Zone' placeholder='Zone  *' value={values.Zone ? values.Zone : ''} required onChange={handleChange} />
+              </div>
+              <div className='address'>
+                <div className='Faddress'>
+                  <i className='fas fa-map-marked-alt'></i>
+                  <input type='text' name='Faddress' id='Faddress' placeholder='Street Address *' required onChange={handleChange} value={values?.Faddress} />
+                </div>
+                <div className='Laddress'>
+                  <i class='fas fa-building'></i>
+                  <input type='text' name='Laddress' id='Laddress' placeholder='Flat / Villa Number  *' required onChange={handleChange} value={values?.Laddress ? values.Laddress : ''} />
+                </div>
+              </div>
+
+              <button className='next' type='submit'>
+                {/* <Link to="/Checkout2" >Next</Link> */}
+                Next
               </button>
             </form>
-            <div className="summary">
-                <h3 >Order summary</h3>
-                <hr />
-                <div className="container-info">
-                    {
-                        cartArray?.map((item,indx)=>{
-                            return(<>
-                            
-                        <div className="info-products">
-                    <div className="info">
-                      <div className="image">
-                        <img
-                          src={item.images[0]}
-                          alt=""
-                        />
-                        <div className="qunt">
-                          <h5>{item.quantity}</h5>
+            <div className='summary'>
+              <h3>Order summary</h3>
+              <hr />
+              <div className='container-info'>
+                {cartArray?.map((item, indx) => {
+                  return (
+                    <>
+                      <div className='info-products'>
+                        <div className='info'>
+                          <div className='image'>
+                            <img src={item.images[0]} alt='' />
+                            <div className='qunt'>
+                              <h5>{item.quantity}</h5>
+                            </div>
+                          </div>
+                          <div className='details'>
+                            <h4>{item.name}</h4>
+                            <p>size: {item.size}</p>
+                            <p>color: {item.color}</p>
+                            <p>buttons:{item.buttons}</p>
+                          </div>
+                        </div>
+                        <div className='price'>
+                          <h4>QAR {item.price}</h4>
                         </div>
                       </div>
-                      <div className="details">
-                        <h4>
-                        {item.name}
-                        </h4>
-                        <p>size: {item.size}</p>
-                        <p>color: {item.color}</p>
-                        <p>buttons:{item.buttons}</p>
-                      </div>
-                    </div>
-                    <div className="price">
-                      <h4>QAR {item.price}</h4>
-                    </div>
-                  </div>
-                            </>)
-                        })
-                    }
-                 
-                  
-                </div>
-
-                <hr />
-                <div className="total">
-                  <h4>Total</h4>
-                  <h4>QAR {total}</h4>
-                </div>
+                    </>
+                  );
+                })}
               </div>
+
+              <hr />
+              <div className='total'>
+                <h4>Total</h4>
+                <h4>QAR {total}</h4>
+              </div>
+            </div>
           </div>
-            
         </div>
-       
-        </section>
-            
-        </>
-    )
+      </section>
+    </>
+  );
 }
 
-export default Checkout
+export default Checkout;
--- src/components/Checkout/Checkout2.jsx
+++ src/components/Checkout/Checkout2.jsx
@@ -1,148 +1,144 @@
-import {React,useState} from "react";
-import { Link } from "react-router-dom";
-import "../../styles/checkout/checkout.css";
+import { React, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../styles/checkout/checkout.css';
 import { useNavigate } from 'react-router-dom';
 
 function Checkout2() {
   const navigate = useNavigate();
-  const InformationData = JSON.parse(
-    window.sessionStorage.getItem("checkout_person_info")
-  );
-  let [values, setValues] = useState(JSON.parse(window.sessionStorage.getItem('checkout_person_info'))?JSON.parse(window.sessionStorage.getItem('checkout_person_info')):{});
+  const InformationData = JSON.parse(window.sessionStorage.getItem('checkout_person_info'));
+  let [values, setValues] = useState(JSON.parse(window.sessionStorage.getItem('checkout_person_info')) ? JSON.parse(window.sessionStorage.getItem('checkout_person_info')) : {});
 
-  const cartArray= JSON.parse(window.sessionStorage.getItem("cart"))
-  const total= JSON.parse(window.sessionStorage.getItem("total"))
-  const handleChange=e=>{
-    setValues({...values,[e.target.name]:e.target.value})
-  }
-  const handleClick=()=>{
+  const cartArray = JSON.parse(window.sessionStorage.getItem('cart'));
+  const total = JSON.parse(window.sessionStorage.getItem('total'));
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  const handleClick = () => {
     window.sessionStorage.setItem('checkout_person_info', JSON.stringify(values));
-    navigate("/Checkout3");
+    navigate('/Checkout3');
     window.scrollTo({
       left: 0,
       top: 0,
       behavior: 'smooth',
     });
-  }
+  };
   return (
     <>
-      <section className="checkout">
-         <div className="nav-container">
-      <div className="nav-info">
-          <div className="left-nav">
-            <Link to='/'><i className="fas fa-home i-home"></i></Link>  
-            <i className="fas fa-angle-right"></i>
-            <Link to='/Cart' className="exat-path"> <span >cart</span></Link> 
-            <i className="fas fa-angle-right"></i> <Link to='/Checkout' className="exat-path"> <span >Shipping</span></Link> 
-            <i className="fas fa-angle-right"></i> <span >Payment</span>
+      <section className='checkout'>
+        <div className='nav-container'>
+          <div className='nav-info'>
+            <div className='left-nav'>
+              <Link to='/'>
+                <i className='fas fa-home i-home'></i>
+              </Link>
+              <i className='fas fa-angle-right'></i>
+              <Link to='/Cart' className='exat-path'>
+                {' '}
+                <span>cart</span>
+              </Link>
+              <i className='fas fa-angle-right'></i>{' '}
+              <Link to='/Checkout' className='exat-path'>
+                {' '}
+                <span>Shipping</span>
+              </Link>
+              <i className='fas fa-angle-right'></i> <span>Payment</span>
+            </div>
           </div>
-         
         </div>
-      </div>
 
-        <div className="lamar-container">
-    
-        <h2><span >Shipping <i className="far fa-check-circle"></i></span> <i className="fas fa-angle-right"></i> <span className='active'>Review & Payments<span className='steps'>2</span></span>
-        <i className="fas fa-angle-right"></i> 
-                  <span>Order Complete <span className='steps'>3</span>
-                  </span></h2>
-            <div className="container-payment">
-              <div className="Shipping-info">
-                  <h4 className="Shipping-title">Shipping Info</h4>
-                <table>
-                  <tbody>
-                    <tr>
-                      <td className="td1">contact</td>
-                      <td className="td2">{InformationData.email}</td>
-                      <td className="td3">
-                        <Link to="/Checkout">change</Link>
-                      </td>
-                    </tr>
-                  </tbody>
-                  <tbody>
-                    <tr>
-                      <td className="td1">Ship to</td>
-                      <td className="td2">
-                        {InformationData.Faddress +
-                          " ," +
-                          InformationData.Laddress}
-                      </td>
-                      <td className="td3">
-                        <Link to="/Checkout">change</Link>
-                      </td>
-                    </tr>
-                  </tbody>
-                  <tbody>
-                    <tr>
-                      <td className="td1">
-                       Method
-                      </td>
-                      <td className="td2">
-                      Free Shipping - Free
-                      </td>
-                      <td className="td3">
-                        <Link to="/Checkout">change</Link>
-                      </td>
-                    </tr>
-                  </tbody>
-                  <tbody>
-                    <tr>
-                      <textarea name="comment" id="comment" cols="30" rows="10" placeholder="Leave A Comment" 
-                      value={values?.comment}
-                      onChange={handleChange}></textarea>
-                    </tr>
-                  </tbody>
-                </table>
-                <button className="next" onClick={handleClick}>continue to payment</button>
-              </div>
-              <div className="summary">
-                <h3 >Order summary</h3>
-                <hr />
-                <div className="container-info">
-                    {
-                        cartArray?.map((item,indx)=>{
-                            return(<>
-                            
-                        <div className="info-products">
-                    <div className="info">
-                      <div className="image">
-                        <img
-                          src={item.images[0]}
-                          alt=""
-                        />
-                        <div className="qunt">
-                          <h5>{item.quantity}</h5>
+        <div className='lamar-container'>
+          <h2>
+            <span>
+              Shipping <i className='far fa-check-circle'></i>
+            </span>{' '}
+            <i className='fas fa-angle-right'></i>{' '}
+            <span className='active'>
+              Review & Payments<span className='steps'>2</span>
+            </span>
+            <i className='fas fa-angle-right'></i>
+            <span>
+              Order Complete <span className='steps'>3</span>
+            </span>
+          </h2>
+          <div className='container-payment'>
+            <div className='Shipping-info'>
+              <h4 className='Shipping-title'>Shipping Info</h4>
+              <table>
+                <tbody>
+                  <tr>
+                    <td className='td1'>contact</td>
+                    <td className='td2'>{InformationData.email}</td>
+                    <td className='td3'>
+                      <Link to='/Checkout'>change</Link>
+                    </td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td className='td1'>Ship to</td>
+                    <td className='td2'>{InformationData.city + ' ,' + InformationData.Zone}</td>
+                    <td className='td3'>
+                      <Link to='/Checkout'>change</Link>
+                    </td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td className='td1'>Method</td>
+                    <td className='td2'>Free Shipping - Free</td>
+                    <td className='td3'>
+                      <Link to='/Checkout'>change</Link>
+                    </td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <textarea name='comment' id='comment' cols='30' rows='10' placeholder='Leave A Comment' value={values?.comment} onChange={handleChange}></textarea>
+                  </tr>
+                </tbody>
+              </table>
+              <button className='next' onClick={handleClick}>
+                continue to payment
+              </button>
+            </div>
+            <div className='summary'>
+              <h3>Order summary</h3>
+              <hr />
+              <div className='container-info'>
+                {cartArray?.map((item, indx) => {
+                  return (
+                    <>
+                      <div className='info-products'>
+                        <div className='info'>
+                          <div className='image'>
+                            <img src={item.images[0]} alt='' />
+                            <div className='qunt'>
+                              <h5>{item.quantity}</h5>
+                            </div>
+                          </div>
+                          <div className='details'>
+                            <h4>{item.name}</h4>
+                            <p>size: {item.size}</p>
+                            <p>color: {item.color}</p>
+                            <p>buttons:{item.buttons}</p>
+                          </div>
+                        </div>
+                        <div className='price'>
+                          <h4>QAR {item.price}</h4>
                         </div>
                       </div>
-                      <div className="details">
-                        <h4>
-                        {item.name}
-                        </h4>
-                        <p>size: {item.size}</p>
-                        <p>color: {item.color}</p>
-                        <p>buttons:{item.buttons}</p>
-                      </div>
-                    </div>
-                    <div className="price">
-                      <h4>QAR {item.price}</h4>
-                    </div>
-                  </div>
-                            </>)
-                        })
-                    }
-                 
-                  
-                </div>
-
-                <hr />
-                <div className="total">
-                  <h4>Total</h4>
-                  <h4>QAR {total}</h4>
-                </div>
+                    </>
+                  );
+                })}
               </div>
-              </div>
-      
 
+              <hr />
+              <div className='total'>
+                <h4>Total</h4>
+                <h4>QAR {total}</h4>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
